3adada2ed598262f65246341473329801c2d3a0a
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2021-05-31 00:02:16 -0300

    .

--- htmlESP32/web_Serverscript/script_Home.js
+++ htmlESP32/web_Serverscript/script_Home.js
@@ -1,3 +1,15 @@
 var Nome = 0;
 var SSID = 0;
 var PWD = 0;
+
+function config() {
+    var text = 0;
+    PWD = document.getElementById("config_PWD").value;
+
+    if (PWD != 1) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
