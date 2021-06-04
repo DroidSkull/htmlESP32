5a29d824e3d5e29dee7b61513ef3df8bbca999fb
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2021-06-04 12:36:49 -0300

    .

--- htmlESP32/web_Serverscript/script_Home.js
+++ htmlESP32/web_Serverscript/script_Home.js
@@ -3,3 +3,15 @@ function logout() {
     window.location.replace("Index.html");
 
 }
+var teste = new Boolean(true)
+
+function mudaBackground() {
+
+    teste = !teste;
+    if (teste === false) {
+        document.getElementById("teste").style.backgroundColor = "#fff";
+    }
+    if (teste === true) {
+        document.getElementById("teste").style.backgroundColor = "#191A1F";
+    }
+}
